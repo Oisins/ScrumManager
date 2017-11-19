a2681d7f110d84f53930d4ea6b9ac66c67ae4ba9
PreXenq <dobberstein.conrad@gmail.com>
2017-11-19 13:28:21 +0100

    login-management

--- app/routes/main.py
+++ app/routes/main.py
@@ -8,5 +8,32 @@ main_blueprint = Blueprint('main', __name__)
 
 @main_blueprint.route("/")
 def index():
+    return render_template("index.html")
+
+
+@main_blueprint.route("/login")
+def login_seite():
     users = User.query.all()
-    return render_template("login.html", users=users)
+    return render_template("login.html", users=users, next=request.args.get('next', ''))
+
+
+@main_blueprint.route("/login/<user_id>")
+def login(user_id):
+    user = User.query.get(user_id)
+    next_url = request.args.get('next', '')
+    if user:
+        login_user(user)
+
+        if next_url:
+            return redirect(next_url)
+        else:
+            return redirect("/")
+    else:
+
+        return "Error"
+
+
+@main_blueprint.route("/logout")
+def logout():
+    logout_user()
+    return redirect("/login")
